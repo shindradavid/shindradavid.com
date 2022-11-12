a9e8f47d837c8dcf55b988d7ec8ebd168e351a8e
shindra david <shindradavid.dev@gmail.com>
2022-11-12 20:18:37 +0300

    Add header to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/work">Work</a>
-	<a href="/contact">Contact</a>
-	<a href="/FAQ">FAQ</a>
-	<a href="/lab">Lab</a>
-	<a href="/blog">Blog</a>
-	<a href="/courses">Courses</a>
-</nav>
+<header class="header">
+	<a href="/" translate="no" class="header__logo">shindra david</a>
+
+	<nav class="header__nav">
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/work">Work</a>
+		<a href="/contact">Contact</a>
+		<a href="/FAQ">FAQ</a>
+		<a href="/lab">Lab</a>
+		<a href="/blog">Blog</a>
+		<a href="/courses">Courses</a>
+	</nav>
+</header>
 
 <svelte:head>
 	<link rel="manifest" href="manifest.json" />
